fecbb9d5f181170bffc421259e4dd31898c6f195
Igor <superpike@mail.ru>
2019-11-13 21:17:19 +0500

    rebuild tests (add snapshots)

--- front/src/components/GamesList/index.test.js
+++ front/src/components/GamesList/index.test.js
@@ -2,68 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { gamesList as GameList } from './index';
 // query utilities:
-import {
-    getByLabelText,
-    getByText,
-    getByTestId,
-    getByDisplayValue,
-    // Tip: all queries are also exposed on an object
-    // called "queries" which you could import here as well
-    wait,
-} from '@testing-library/dom'
+// import {
+//     getByLabelText,
+//     getByText,
+//     getByTestId,
+//     getByDisplayValue,
+//     // Tip: all queries are also exposed on an object
+//     // called "queries" which you could import here as well
+//     wait,
+// } from '@testing-library/dom'
 // adds special assertions like toHaveTextContent
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 
-const store = {
-
-}
-
 describe("Testing gamesList", () => {
+    const games = [
+        {
+            name: 'qwert',
+            id: 1
+        },
+        {
+            name: 'qwert2',
+            id: 2
+        }
+    ]
     describe("Is label", () => {
         test('should be label for list', () => {
-            const div = document.createElement('div');
-            ReactDOM.render(
+            const { container } = render(
                 <GameList
-                    games={[
-                        {
-                            name: 'qwert',
-                            id: 1
-                        },
-                        {
-                            name: 'qwert2',
-                            id: 2
-                        }
-                    ]}
-                    activeGame={{ name: 'qwert2', id: 1 }}
+                    games={games}
+                    activeGame={{ ...games[0] }}
                     dispatch={() => { }}
                     isFavorite={false}
-                />, div);
-            const label = div.querySelector('label');
-            expect(label.innerHTML).toBe('games for you');
+                />
+            );
+            expect(container).toMatchSnapshot();
         })
     })
     describe("List of the games", () => {
         test('should be chosen an active game', () => {
-            const div = document.createElement('div');
-            ReactDOM.render(
+            const { container } = render(
                 <GameList
-                    games={[
-                        {
-                            name: 'qwert',
-                            id: 1
-                        },
-                        {
-                            name: 'qwert2',
-                            id: 2
-                        }
-                    ]}
-                    activeGame={{ name: 'qwert2', id: 2 }}
+                    games={games}
+                    activeGame={{ ...games[1] }}
                     dispatch={() => { }}
                     isFavorite={false}
-                />, div);
-            const input = div.querySelector('input');
-            expect(input.value).toBe('qwert2');
+                />
+            );
+            expect(container).toMatchSnapshot();
         })
         test('after click should appears a list', async () => {
             const { container, getByText } = render(<GameList
@@ -83,37 +69,32 @@ describe("Testing gamesList", () => {
             />)
             const button = container.querySelector('.fa-chevron-down').parentElement;
             button.click();
-            await wait(() => {
-                expect(getByText('qwert')).toBeInTheDocument();
-                expect(getByText('qwert2')).toBeInTheDocument();
-            }
-            );
+            expect(getByText('qwert')).toBeInTheDocument();
+            expect(getByText('qwert2')).toBeInTheDocument();
         })
     })
     describe("Favorites", () => {
         test('should render favorite icon', () => {
-            const div = document.createElement('div');
-            ReactDOM.render(
+            const { container } = render(
                 <GameList
-                    games={[]}
-                    activeGame={{ name: 'qwert', id: 1 }}
+                    games={games}
+                    activeGame={{ ...games[0] }}
                     dispatch={() => { }}
                     isFavorite={true}
-                />, div);
-            const fav = div.querySelector('.fa-star');
-            expect(fav.classList.contains('fas')).toBe(true);
+                />
+            );
+            expect(container).toMatchSnapshot();
         })
         test('should render non-favorite icon', () => {
-            const div = document.createElement('div');
-            ReactDOM.render(
+            const { container } = render(
                 <GameList
-                    games={[]}
-                    activeGame={{ name: 'qwert', id: 1 }}
+                    games={games}
+                    activeGame={{ ...games[0] }}
                     dispatch={() => { }}
                     isFavorite={false}
-                />, div);
-            const fav = div.querySelector('.fa-star');
-            expect(fav.classList.contains('far')).toBe(true);
+                />
+            );
+            expect(container).toMatchSnapshot();
         })
     })
 })
