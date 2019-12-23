5ab1594f090a96d0f81ad6d0eb908e6d888d9496
Igor <superpike@mail.ru>
2019-12-23 14:45:06 +0500

    add structure for internalization feature

---
+++ front/src/i18n.js
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './languages/en.json';
+import ru from './languages/ru.json';
+import cz from './languages/cz.json';
+
+i18n.use(initReactI18next).init({
+  // we init with resources
+  resources: {
+    en: {
+      translations: en,
+    },
+    ru: {
+      translations: ru,
+    },
+    cz: {
+      translations: cz,
+    },
+  },
+  fallbackLng: 'cz',
+  debug: true,
+
+  // have a common namespace used around the full app
+  ns: ['translations'],
+  defaultNS: 'translations',
+
+  keySeparator: false, // we use content as keys
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
